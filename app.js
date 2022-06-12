d8a7aeadd51692f435d4e24c419377eb29520537
Stephanie Wong <stephanie@stephs-mbp.lan>
2022-06-11 22:41:45 -0400

    menu transitions

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const mobile_menu = document.querySelector('.container .navbar .list ul');
 const menu_item = document.querySelectorAll('.container .navbar .list ul li a');
 const header = document.querySelector('.header .container');
 
+// open and close menu manually
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
+});
+
+// the menu goes away after link is clickes
+menu_item.forEach( link => {
+	link.addEventListener('click', ()=> {
+	  	hamburger.classList.remove('show');
+		mobile_menu.classList ="initial";
+	})
 });
