0465f95869dcbc4597af86513b5242d0d711a50c
Stephanie Wong <stephanie@stephs-mbp.lan>
2022-06-11 23:15:55 -0400

    restyle exp section. Tab pannels function created along with html portion

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Navbar and Hamburger menu
 const hamburger = document.querySelector('.container .navbar .list .hamburger');
 const mobile_menu = document.querySelector('.container .navbar .list ul');
 const menu_item = document.querySelectorAll('.container .navbar .list ul li a');
@@ -26,3 +27,24 @@ document.addEventListener('scroll', () => {
 		header.style.backgroundColor = 'transparent';
 	}
 });
+
+// Work experiences tab 
+function openExp(evt, experience) {
+
+	// declare variables
+	var i, tabcontent, tablinks;
+
+	
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i ++) 
+		tabcontent[i].style.display = "none";
+
+	// remove active class
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i ++) 
+		tablinks[i].className = tablinks[i].className.replace("active", "");
+
+	// show current tab and add active class to the link opened in tab
+	document.getElementById(experience).style.display = "block";
+	evt.currentTarget.classNmae += "active";
+}
